2ae73609456af21b2f5a19ac2d0a0fca34785af4
sipandrei <pontos.serban@gmail.com>
2022-01-01 01:25:47 +0200

    added validations and relations between models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  belongs_to :author
+  has_many :comments
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+  validates :name, presence: true
+  has_many :articles
 end
