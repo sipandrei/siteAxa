bdce370df4ac8d95fd824ad850861d558ee6169d
sipandrei <pontos.serban@gmail.com>
2022-01-02 13:09:42 +0200

    basic devise integration plus admin collumn for user

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   belongs_to :author
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :views
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'registrations'}
+  resources :users, only: %i[index show]
   root 'articles#index'
   resources :authors
   resources :articles
