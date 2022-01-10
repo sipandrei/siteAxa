ef645fee41d499050c9f98ac3235d05b6059e4a2
sipandrei <pontos.serban@gmail.com>
2022-01-10 10:54:39 +0200

    added article categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   belongs_to :author
+  belongs_to :category
   has_many :comments, dependent: :destroy
   has_one_attached :banner
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_110804) do
+ActiveRecord::Schema.define(version: 2022_01_10_082958) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,10 +46,18 @@ ActiveRecord::Schema.define(version: 2022_01_02_110804) do
     t.integer "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
     t.index ["author_id"], name: "index_articles_on_author_id"
+    t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
   create_table "authors", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -90,4 +98,5 @@ ActiveRecord::Schema.define(version: 2022_01_02_110804) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "authors"
+  add_foreign_key "articles", "categories"
 end
