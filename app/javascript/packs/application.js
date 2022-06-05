a7c21b148a2d2f4c0fdfea3ebcd961ff6eb5c42d
sipandrei <pontos.serban@gmail.com>
2022-06-05 10:49:56 +0300

    directory structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import "src";
 
 Rails.start();
 Turbolinks.start();
@@ -14,4 +15,3 @@ ActiveStorage.start();
 
 require("trix");
 require("@rails/actiontext");
-require("src/burgerMenu");
